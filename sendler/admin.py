aca3aab145145ab87d7147b7626b9a41d9aa040b
Viktor Nikulaev <viktor_1611@mail.ru>
2024-10-01 23:50:32 +0300

    add admin

--- sendler/admin.py
+++ sendler/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from sendler.models import Client, Message, MailSettings, Log
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email')
+    list_filter = ('email',)
+    search_fields = ('first_name', 'last_name', 'email')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'message_title', 'created_at', 'update_at')
+    search_fields = ('message_title',)
+
+
+@admin.register(MailSettings)
+class MailSettingsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_time_send', 'periodicity', 'status', 'client', 'message')
+    list_filter = ('periodicity', 'status', 'client', 'message')
+    search_fields = ('status', 'client', 'message')
+
+
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date_last_attempt', 'attempt_status', 'server_response', 'mailing')
+    list_filter = ('attempt_status', 'server_response', 'mailing')
+    search_fields = ('server_response',)
+
+
+
